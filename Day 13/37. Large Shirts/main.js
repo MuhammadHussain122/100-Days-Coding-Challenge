8f7d8384260663be62917a504c7054f3564fcd2c
Muhammad Hussain <muhammadhucen830@gmail.com>
2024-03-14 14:05:26 +0500

    Hundred Days Coding Challenge.

---
+++ Day 13/37. Large Shirts/main.js
@@ -0,0 +1,14 @@
+/* Large Shirts:
+Modify the make_shirt() function so that shirts are large
+by default with a message that reads I love TypeScript. Make a large shirt and a
+medium shirt with the default message, and a shirt of any size with a different
+message
+*/
+function make_shirt(size, message) {
+    if (size === void 0) { size = "large"; }
+    if (message === void 0) { message = "I love Typescript"; }
+    console.log("I made a ".concat(size, " size t-shirt with ").concat(message, " printed on it"));
+}
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "let's start coding");
